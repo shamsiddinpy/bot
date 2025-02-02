2083710857af1ce05dbd08c064c9e5d4b8a6c640
shamsiddindaminov <shamsiddinpython215@gmail.com>
2025-02-02 17:26:18 +0500

    telegram botdan kelayotgan nomerni (90)-000-00-00 formatda api bilan solishitirdim

--- bot/handler/utls/main_jshshir_filter.py
+++ bot/handler/utls/main_jshshir_filter.py
@@ -9,20 +9,19 @@ def is_jshshir_number_filter(number: str) -> bool:
 def format_phone_number(phone: str) -> str:
     """Telefon raqamlarini (93)-908-12-17 formatiga o'tkazish"""
     digits = re.findall(r'\d+', phone)
-    digits = ''.join(digits)  # Ro‘yxatni stringga aylantiramiz
-
-    if len(digits) == 9:  # Masalan: 908121217
+    digits = ''.join(digits)
+    if len(digits) == 9:
         return f"(9{digits[0]})-{digits[1:4]}-{digits[4:6]}-{digits[6:]}"
     elif len(digits) == 12 and digits.startswith("998"):  # Masalan: 998908121217
         return f"({digits[3:5]})-{digits[5:8]}-{digits[8:10]}-{digits[10:]}"
 
-    return phone  # Agar formatga to‘g‘ri kelmasa, hech qanday o‘zgarish qilinmaydi
+    return phone
 
 
 def normalize_phone(phone: str) -> str:
     """Faqat oxirgi 9 ta raqamni olish va keyin formatlash"""
-    digits = ''.join(filter(str.isdigit, phone))[-9:]  # So‘nggi 9 ta raqamni olish
-    return format_phone_number(digits)  # Formatga moslashtirish
+    digits = ''.join(filter(str.isdigit, phone))[-9:]
+    return format_phone_number(digits)
 
 
 def format_date(date_str: str) -> str:
